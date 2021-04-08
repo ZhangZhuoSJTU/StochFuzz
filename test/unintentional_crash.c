264443d6961c5427aa361de6d48b9b76f48908b3
Zhuo Zhang <zhan3299@purdue.edu>
2021-04-08 14:03:59 -0400

    add uninterntional_crash test case

---
+++ test/unintentional_crash.c
@@ -0,0 +1,53 @@
+#define ASMSTR(S) "\t" S "\n"
+
+int main(int argc, char **argv) {
+    asm volatile(
+        ASMSTR(".intel_syntax noprefix")
+
+        ASMSTR("test rdi, rdi;")
+        ASMSTR("jne Y;")
+        ASMSTR("jmp A;")
+        ASMSTR("jmp Z;")
+        ASMSTR("jmp A;")
+        ASMSTR("jmp Z;")
+        ASMSTR("jmp A;")
+        ASMSTR("jmp Z;")
+        ASMSTR("jmp A;")
+        ASMSTR("jmp Z;")
+        ASMSTR("jmp A;")
+        ASMSTR("jmp Z;")
+        ASMSTR("jmp A;")
+        ASMSTR("jmp Z;")
+
+        ASMSTR(".global Y")
+        ASMSTR("Y:")
+        ASMSTR("xor rbx, rbx")
+        ASMSTR("mov bx, word ptr [X];")
+        ASMSTR("sub rbx, 0xf1dc;")
+        ASMSTR("push rbx;")
+        ASMSTR("lea r8, [rip + A];")
+
+        ASMSTR(".global A")
+        ASMSTR("A:")
+        ASMSTR("pop r9;")
+        ASMSTR("add r8, r9;")
+        ASMSTR("call r8;")
+
+        ASMSTR(".global Z")
+        ASMSTR("Z:")
+        ASMSTR("call A;")
+        ASMSTR(".global X")
+        ASMSTR("X:")
+        ASMSTR("jmp A;")
+
+        ASMSTR(".global B")
+        ASMSTR("B:")
+        ASMSTR("mov rax, 60;")
+        ASMSTR("mov rdi, 0;")
+        ASMSTR("syscall;")
+
+        ASMSTR("ret;")
+
+        ASMSTR(".att_syntax;")
+        );
+}
