f0a3eb91227d2d5a966c0c7a091e0c55084680d9
Zhuo Zhang <zhan3299@purdue.edu>
2021-05-24 17:56:58 -0400

    v1.1.0: add license

--- src/config.h
+++ src/config.h
@@ -1,3 +1,21 @@
+/*
+ * config.h
+ * Copyright (C) 2021 Zhuo Zhang, Xiangyu Zhang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
@@ -20,7 +38,7 @@
  * Tool name and version
  */
 #define OURTOOL "stoch-fuzz"
-#define VERSION "1.0.0"
+#define VERSION "1.1.0"
 
 /*
  * Magic string to indicate the file is patched
--- src/rewriter_handlers/handler_cjmp.c
+++ src/rewriter_handlers/handler_cjmp.c
@@ -1,3 +1,21 @@
+/*
+ * handler_cjmp.c
+ * Copyright (C) 2021 Zhuo Zhang, Xiangyu Zhang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define REVENT z_capstone_is_cjmp
 #define RHANDLER __rewriter_cjmp_handler
 
