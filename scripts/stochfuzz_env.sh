f27e0207fd6cc0c8ca419e04967e3ac4e41a31ed
Zhuo Zhang <zhan3299@purdue.edu>
2021-05-24 15:21:25 -0400

    minor fix

--- scripts/stochfuzz_env.sh
+++ scripts/stochfuzz_env.sh
@@ -6,10 +6,12 @@ libunwind_path="$stochfuzz_dir/libunwind/install/lib/libunwind.so"
 
 if [ ! -f $libstochfuzzRT_path ]; then
     echo "libstochfuzzRT.so not found!"
+    exit 1
 fi
 
 if [ ! -f $libunwind_path ]; then
     echo "libunwind.so not found!"
+    exit 1
 fi
 
 export STOCHFUZZ_PRELOAD=$libstochfuzzRT_path:$libunwind_path
